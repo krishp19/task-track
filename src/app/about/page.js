2d2236929b7ecf54678f04206c04c71e5c856f98
Krish Prasad <121675445+krishp19@users.noreply.github.com>
2024-07-18 12:23:09 +0530

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,15 +1,8 @@
 "use client"
 import { resolve } from "styled-jsx/css"
 
-async function takeTime(params) {
-    await new Promise( (resolve) => {
-        setTimeout(resolve, 3000);
-    });
-}
 
 export default async function About() {
-    await takeTime();
-    throw new Error("This is manual error");
     return(
         <div>
             <h1>About</h1>
